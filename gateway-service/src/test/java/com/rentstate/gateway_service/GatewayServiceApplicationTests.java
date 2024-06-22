7223e32419f3ed0c612f4002bd90f537cdeac0ee
Nirvana Garcia <84489764+nirvagarcia@users.noreply.github.com>
2024-06-22 18:55:54 -0500

    feat: adding gateway_service test

--- gateway-service/src/test/java/com/rentstate/gateway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/rentstate/gateway_service/GatewayServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.rentstate.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
