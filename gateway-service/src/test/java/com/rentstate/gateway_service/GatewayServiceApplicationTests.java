2dc4ba2e0ceef7503b7193d120b692df5d749dd1
Sebastián Ramírez Méndez <149857901+yorusaurio@users.noreply.github.com>
2024-06-22 19:01:07 -0500

    fix: gateway_service

--- gateway-service/src/test/java/com/rentstate/gateway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/rentstate/gateway_service/GatewayServiceApplicationTests.java
@@ -2,7 +2,8 @@ package com.rentstate.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest;
+
+
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
