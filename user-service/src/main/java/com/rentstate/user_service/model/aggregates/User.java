63a6487ddd662c9a6ec725722974d036605aa2dd
GabbrM <115942603+GabbrM@users.noreply.github.com>
2024-10-30 00:19:13 -0500

    feat: added new parameters to user entity

--- user-service/src/main/java/com/rentstate/user_service/model/aggregates/User.java
+++ user-service/src/main/java/com/rentstate/user_service/model/aggregates/User.java
@@ -23,7 +23,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name ="users")
-public class User implements UserDetails {
+public class  User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,6 +37,12 @@ public class User implements UserDetails {
     @Size(min=2, max=50, message = "The last name must have at least 2 characters and a maximum of 50")
     private String lastName;
 
+    private String district;
+
+    private Date birthDate;
+
+    private String description = "Here you can write a description about yourself.";
+
     private String email;
 
     @NotNull
@@ -46,34 +52,91 @@ public class User implements UserDetails {
     @NotNull
     private String password;
 
-    private Integer age = 18;
 
-    private String gender="reserved";
-
-    private String description = "Here you can write a description about yourself.";
-
-    private Boolean isPremium = false;
+    private String gender;
 
     private String photoUrl = "";
 
     private Role role;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> contacts = new ArrayList<>();
+
+    private Double latitude;
+    private Double longitude;
+    private String department;
+    private String city;
+    private String address;
+
+    @Column(unique = true)
+    private String dni;
+
+    private String phone;
+
+    private Boolean premium;
+    private Boolean chatNewMessage;
+    private Boolean newPropertyNear;
+
+    @Convert(converter = CoverageAreaInterestConverter.class)
+    @Column(columnDefinition = "TEXT")
+    private List<List<Coordinates>> coverageAreaInterest = new ArrayList<>();
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_favorite_properties", joinColumns = @JoinColumn(name = "user_id"))
+    private List<Long> favoriteProperties = new ArrayList<>();
+
+
+    @Convert(converter = UserNeedsConverter.class)
+    @Column(columnDefinition = "TEXT")
+    private Map<String, Object> userNeeds;
+
+
+
    public User(RegisterRequest registerRequest){
-        name = registerRequest.getName();
-        lastName = registerRequest.getLastName();
-        username = registerRequest.getUsername();
-        password = registerRequest.getPassword();
-        role=Role.USER;
-    }
+       name = registerRequest.getName();
+       lastName = registerRequest.getLastName();
+       username = registerRequest.getUsername();
+       email = registerRequest.getEmail();
+       district = registerRequest.getDistrict();
+       birthDate = registerRequest.getBirthDate();
+       gender = registerRequest.getGender();
+       password = registerRequest.getPassword();
+       role = registerRequest.getRole();
+       contacts = new ArrayList<>();
+       latitude = registerRequest.getLatitude();
+       longitude = registerRequest.getLongitude();
+       photoUrl = registerRequest.getPhotoUrl();
+       department = registerRequest.getDepartment();
+       city = registerRequest.getCity();
+       address = registerRequest.getAddress();
+       phone = registerRequest.getPhone();
+       dni = registerRequest.getDni();
+   }
 
     public void updateUser(UpdateUserResource resource){
         this.name = resource.getName();
         this.lastName = resource.getLastName();
-        this.age = resource.getAge();
         this.gender = resource.getGender();
+        this.district = resource.getDistrict();
+        this.email = resource.getEmail();
         this.description = resource.getDescription();
-        this.isPremium = resource.getIsPremium();
         this.photoUrl = resource.getPhotoUrl();
+        this.role = resource.getRole();
+        this.latitude = resource.getLatitude();
+        this.longitude = resource.getLongitude();
+        this.department = resource.getDepartment();
+        this.city = resource.getCity();
+        this.address = resource.getAddress();
+        this.dni = resource.getDni();
+        this.phone = resource.getPhone();
+        this.premium = resource.getPremium();
+        this.chatNewMessage = resource.getChatNewMessage();
+        this.newPropertyNear = resource.getNewPropertyNear();
+        this.coverageAreaInterest = resource.getCoverageAreaInterest();
+        this.favoriteProperties = resource.getFavoriteProperties();
+        this.userNeeds = (Map<String, Object>) resource.getUserNeeds();
+
     }
 
 
